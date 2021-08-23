6340793cd15b3416561e47742953e114efdc9720
Cheng Gang <chenggangpro@gmail.com>
2021-08-23 22:00:28 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # you-get-tool
 you-get tool 
+
+
+>  tools for you-get download video/playlist with mp4 format ,support multi urls  
---
+++ src/test/java/pro/chenggang/project/yougettool/YouGetToolApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.chenggang.project.yougettool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouGetToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
